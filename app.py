d6ab927638aca685916b49c35c01a37fffe97067
Ritesh Kumar Verma <ritesh782002@gmail.com>
2026-07-20 21:22:34 +0530

    added feature to view from local storage and edit existing data

--- app.py
+++ app.py
@@ -1,155 +1,30 @@
 
 import streamlit as st
-import json
+from components.User_Info import User_Info
 from streamlit_local_storage import LocalStorage
-
-local_storage = LocalStorage()
-
-
 st.set_page_config(layout='wide')
 
 st.title("AI Resume Optimizer")
 
 
 
-def save_data():
-    data = json.dumps(st.session_state.d)
-    
-    local_storage.setItem(
-        "resume_data",
-        data
-    )
 
 
-def user_info():
-    st.subheader("Enter your Details")
-    
-    if "d" not in st.session_state:
-        st.session_state.d = {}
 
-    if "projects" not in st.session_state:
-        st.session_state.projects = []
-        
-        
-
-    
-    basic_left_col , basic_right_col = st.columns(2)
-    
-    #####Basic Info#####
-    basic_info = dict()
-    
-    with basic_left_col:
-        basic_info['name'] = st.text_input("Enter your name")
-        basic_info['phone_number'] = st.text_input("Enter your phone number")
-        basic_info['email'] = st.text_input("Enter your email")
-        basic_info['address'] = st.text_area("Enter your address")
-    with basic_right_col:
-        basic_info['profile_link'] = st.text_input("Enter your profile/portfolio link")
-        basic_info['linkedin'] = st.text_input("Enter your linked url")
-        basic_info['github'] = st.text_input("Enter your github url")
-    basic_info['summary'] = st.text_area("Enter you summary")
-    
-    st.write(basic_info)
-    
-    st.markdown('---')
-    
-    st.session_state.d['basic_info'] = basic_info
-    #####Technical Skills#####
-
-    
-    st.subheader("Technical skills")
-    technical_left_col, technical_right_col = st.columns(2)
-    
-    technical_skill = dict()
-    
-    with technical_left_col:
-        
-        technical_skill['programming_language'] = st.text_input("Programming language")
-        
-        technical_skill['frontend'] = st.text_input("Frontend skills")
-        
-        technical_skill['backend'] = st.text_input("Backend skills")
-        
-    with technical_right_col:
-        technical_skill['database'] = st.text_input("Database knowledge")
-        
-        technical_skill['ai_ml_data_science'] = st.text_input("AI Info")
-        
-        
-        technical_skill['tools'] = st.text_input("Tools like Vsc, IntelliJ")
-        
-    
-        
-    st.session_state.d['technical_skill'] = technical_skill
-    
-    
-    
-    st.markdown('---')
-
-    #####Education Info#####
-    st.subheader("Education Details")
-    
-    education = dict()
-    
-    education_left_col , education_right_col = st.columns(2)
-
-    with education_left_col:
-        education['institute_name'] = st.text_input("Enter institute name")
-        education['degree_certificate_name'] = st.text_input("Enter Degree/Certificate Name")
-    with education_right_col:
-        education['location'] = st.text_input("Enter Address of Institute")
-        education['duration'] = st.text_input("Enter duration in format 2019-2023")
-
-    st.session_state.d['education'] = education
-    st.markdown('---')
-    
-    
-    #####Project Info#####
-
-    
-    st.subheader("Project Details")
-    project_details()
-    
-    st.session_state.d['projects'] = st.session_state.projects
-    
-    if st.button("Create Resume"):
-        st.write(st.session_state.d)
-        save_data()
-    
-
-def project_details():
-    with st.form("project_form",clear_on_submit=True):
-        title = st.text_input("Project Title")
-        tech = st.text_input("Technologies Used")
-        year = st.text_input("Project Year")
-        frontend = st.text_input("Frontend Code URL")
-        backend = st.text_input("Backend Code URL")
-        live = st.text_input("Live Demo URL")
-        description = st.text_area("Enter your description",placeholder="Enter points seprated with comma")
-
-        submitted = st.form_submit_button("Add Project")
-
-        if submitted:
-            st.session_state.projects.append({
-                "title": title,
-                "tech_used": tech,
-                "project_year": year,
-                "frontend_code_url": frontend,
-                "backend_code_url": backend,
-                "live_link": live,
-                "description" : description
-            })
-            st.success("Project added Successfully")
-    
-    
+  
     
 
 
 
 if __name__ == '__main__':
     
+    local_storage = LocalStorage()
+    
+    user_Info = User_Info()
+    
     data = local_storage.getItem("resume_data")
-    if not data:
-        user_info()
+    
+    # user_Info.display_details()
         
-    st.write("Resume Data Available")
+    user_Info.user_info()
+         
---
+++ components/User_Info.py
@@ -0,0 +1,191 @@
+import streamlit as st
+from streamlit_local_storage import LocalStorage
+import json
+class User_Info:
+    
+    def __init__(self):
+        
+        self.local_storage = LocalStorage()
+        data = self.load_data()
+        
+        if data:
+            st.session_state.d = data
+            st.session_state.projects = data['projects']
+        else:
+            st.session_state.d = {
+                "basic_info": {
+                    "name": "",
+                    "phone_number": "",
+                    "email": "",
+                    "address": "",
+                    "profile_link": "",
+                    "linkedin": "",
+                    "github": "",
+                    "summary": ""
+                },
+                "technical_skill": {
+                    "programming_language": "",
+                    "frontend": "",
+                    "backend": "",
+                    "database": "",
+                    "ai_ml_data_science": "",
+                    "tools": ""
+                },
+                "education": {
+                    "institute_name": "",
+                    "degree_certificate_name": "",
+                    "location": "",
+                    "duration": ""
+                },
+                "projects": []
+                }
+            st.session_state.projects = [{
+                    "title": "",
+                    "tech_used": "",
+                    "project_year": "",
+                    "frontend_code_url": "",
+                    "backend_code_url": "",
+                    "live_link": "",
+                    "description" : ""
+                }]
+
+        # if "projects" not in st.session_state:
+        
+
+    
+    def save_data(self):
+        data = json.dumps(st.session_state.d)        # d is dictionary
+        
+        self.local_storage.setItem(
+            "resume_data",
+            data
+        )
+    def load_data(self):
+        data = self.local_storage.getItem("resume_data")
+        if data is None:
+            return {}
+        data = json.loads(data)
+        return data 
+        
+    def display_details(self):
+        st.write(self.load_data())
+    
+    
+    def project_details(self,project):
+        with st.form(key=f'project_{project['title']}',clear_on_submit=True):
+            title = st.text_input("Project Title",value=project['title'])
+            tech_used = st.text_input("Technologies Used",value=project['tech_used'])
+            project_year = st.text_input("Project Year",value=project['project_year'])
+            frontend_code_url = st.text_input("Frontend Code URL",value=project['frontend_code_url'])
+            backend_code_url = st.text_input("Backend Code URL",value=project['backend_code_url'])
+            live_link = st.text_input("Live Demo URL",value=project['live_link'])
+            description = st.text_area("Enter your description",placeholder="Enter points seprated with comma",value=project['description'])
+            submitted = st.form_submit_button("Add Project")
+
+            if submitted:
+                st.session_state.projects.append({
+                    "title": title,
+                    "tech_used": tech_used,
+                    "project_year": project_year,
+                    "frontend_code_url": frontend_code_url,
+                    "backend_code_url": backend_code_url,
+                    "live_link": live_link,
+                    "description" : description
+                })
+                st.success("Project added Successfully")
+        
+    def user_info(self):
+        
+        st.subheader("Enter your Details")
+        
+        basic_left_col , basic_right_col = st.columns(2)
+        
+        #####Basic Info#####
+        basic_info = dict()
+        # st.write(st.session_state.d)
+        with basic_left_col:
+            basic_info['name'] = st.text_input("Enter your name",value=st.session_state.d['basic_info']['name'])
+            basic_info['phone_number'] = st.text_input("Enter your phone number",value=st.session_state.d['basic_info']['phone_number'])
+            basic_info['email'] = st.text_input("Enter your email",value=st.session_state.d['basic_info']['email'])
+            basic_info['address'] = st.text_area("Enter your address",value=st.session_state.d['basic_info']['address'])
+        with basic_right_col:
+            basic_info['profile_link'] = st.text_input("Enter your profile/portfolio link",value=st.session_state.d['basic_info']['profile_link'])
+            basic_info['linkedin'] = st.text_input("Enter your linked url",value=st.session_state.d['basic_info']['linkedin'])
+            basic_info['github'] = st.text_input("Enter your github url",value=st.session_state.d['basic_info']['github'])
+        basic_info['summary'] = st.text_area("Enter you summary",value=st.session_state.d['basic_info']['summary'])
+        
+        
+        st.markdown('---')
+        
+        st.session_state.d['basic_info'] = basic_info
+        #####Technical Skills#####
+
+        
+        st.subheader("Technical skills")
+        technical_left_col, technical_right_col = st.columns(2)
+        
+        technical_skill = dict()
+        
+        with technical_left_col:
+            
+            technical_skill['programming_language'] = st.text_input("Programming language",value=st.session_state.d['technical_skill']['programming_language'])
+            
+            technical_skill['frontend'] = st.text_input("Frontend skills",value=st.session_state.d['technical_skill']['frontend'])
+            
+            technical_skill['backend'] = st.text_input("Backend skills",value=st.session_state.d['technical_skill']['backend'])
+            
+        with technical_right_col:
+            technical_skill['database'] = st.text_input("Database knowledge",value=st.session_state.d['technical_skill']['database'])
+            
+            technical_skill['ai_ml_data_science'] = st.text_input("AI Info",value=st.session_state.d['technical_skill']['ai_ml_data_science'])
+            
+            
+            technical_skill['tools'] = st.text_input("Tools like Vsc, IntelliJ",value=st.session_state.d['technical_skill']['tools'])
+            
+        
+            
+        st.session_state.d['technical_skill'] = technical_skill
+        
+        
+        
+        st.markdown('---')
+
+        #####Education Info#####
+        st.subheader("Education Details")
+        
+        education = dict()
+        
+        education_left_col , education_right_col = st.columns(2)
+
+        with education_left_col:
+            education['institute_name'] = st.text_input("Enter institute name", value=st.session_state.d['education']['institute_name'])
+            education['degree_certificate_name'] = st.text_input("Enter Degree/Certificate Name", value=st.session_state.d['education']['degree_certificate_name'])
+        with education_right_col:
+            education['location'] = st.text_input("Enter Address of Institute", value=st.session_state.d['education']['location'])
+            education['duration'] = st.text_input("Enter duration in format 2019-2023", value=st.session_state.d['education']['duration'])
+
+        st.session_state.d['education'] = education
+        st.markdown('---')
+        
+        
+        #####Project Info#####
+
+        
+        st.subheader("Project Details")
+        
+        for project in st.session_state.projects:
+            self.project_details(project)
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        if st.button("Create Resume"):
+            st.write(st.session_state.d)
+            self.save_data()
+            
+            
